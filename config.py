c92e0eda9e4cfb9f49a546b66beffe167374c8c3
Sandeep sharma <rs3268840@gmail.com>
2025-06-21 09:36:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 API_URL = getenv("API_URL", 'https://my-api-lc2j.onrender.com') #youtube song url
-API_KEY = getenv("API_KEY", 'zefron_api_key) # youtube song api ke
+API_KEY = getenv("API_KEY", 'zefron_api_key') # youtube song api ke
 
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
